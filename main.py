b6491770f61e493a78b6fb4ce3963d8199002c5b
FastFingertips <bugra.coskun@outlook.com>
2022-03-01 21:34:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ def getBackgroundImage(_profileDom):
 		print(f'Process: {getBackgroundImage.__name__}')
 
 	backgroundPath = 'images/background'
-	backgroundName = f'{getUsername(_profileDom)}-bg-{getCurrentSession()}'
+	backgroundName = f'{getUsername(_profileDom)}-bg-{getCurrentSession(8)}'
 	try:
 		backgroundImageUrl = _profileDom.find("div", {"class":"header-background header-background--has-image"})["style"][22:-2]
 		doDownloadImage(backgroundPath, backgroundName, backgroundImageUrl)
@@ -188,7 +188,7 @@ def getUserAvatar(_profileDom):
 		print(f'Process: {getUserAvatar.__name__}')
 
 	avatarPath = 'images/avatar'
-	avatarName =  f'{getUsername(_profileDom)}-av-{getCurrentSession()}'
+	avatarName =  f'{getUsername(_profileDom)}-av-{getCurrentSession(8)}'
 	defaultAvatarId = "818148bf682d429dc215c1705eb27b98"
 	# defaultImageUrl:("https://lastfm.freetls.fastly.net/i/u/avatar170s/818148bf682d429dc215c1705eb27b98.png") 
 	profileAvatarUrl = _profileDom.find("meta", property="og:image")["content"]
@@ -247,19 +247,28 @@ def getDisplayName(_profileDom):
 	profileDisplayName= _profileDom.find("span", {"class":"header-title-display-name"})
 	return profileDisplayName.text.strip()
 
-def getCurrentSession():
+def getCurrentSession(get_length=None):
 	if defStatus:
 		print(f'Process: {getCurrentSession.__name__}')
+	
+	'''
+		get_length ? = ? 
+		get_length 8 = %Y%m%d
+		get_length 6 = %Y%m
+		get_length 4 = %Y
+	'''
+	session = datetime.now().strftime('%Y%m%d')
+	if get_length != None:
+		print(session[:get_length])	
+	return session
 
-	return datetime.now().strftime('%Y%m%d')
-
-def getUserFollowingCount(_followingDom):
+def getUserFollowingCount(following_dom):
 	if defStatus:
 		print(f'Process: {getUserFollowingCount.__name__}')
 
 	while True:
 		try:
-			topHeader = _followingDom.find("h1", {"class":"content-top-header"}).text # Path
+			topHeader = following_dom.find("h1", {"class":"content-top-header"}).text # Path
 			userFollowing = topHeader[topHeader.find("(")+1:topHeader.find(")")] # Parantez arası değeri
 			try:
 				userFollowing = int(userFollowing) # Sayı değilse
@@ -269,13 +278,13 @@ def getUserFollowingCount(_followingDom):
 		except:
 			continue	
 
-def getUserFollowersCount(_followersDom):
+def getUserFollowersCount(followers_dom):
 	if defStatus:
 		print(f'Process: {getUserFollowersCount.__name__}')
 	
 	while True:
 		try:
-			topHeader = _followersDom.find("h1", {"class":"content-top-header"}).text # Path
+			topHeader = followers_dom.find("h1", {"class":"content-top-header"}).text # Path
 			userFollowers = topHeader[topHeader.find("(")+1:topHeader.find(")")] # Parantez arası değeri
 			try:
 				userFollowers = int(userFollowers) # Sayı değilse
@@ -285,76 +294,72 @@ def getUserFollowersCount(_followersDom):
 		except:
 			continue
 
-def getUserFollowing(_followingDom):
+def getUserFollowing(following_dom):
 	if defStatus:
 		print(f'Process: {getUserFollowing.__name__}')
 
 	followingDict = {}
-	currentFollowingPageDom = _followingDom
-	username = getUsername(currentFollowingPageDom)
 	while True:
-		following = currentFollowingPageDom.find_all(attrs={"class": "user-list-name"})
-		for f in following: # Bir sayfada max 30
-			f_username = f.text.strip()
-			followingDict[f_username] = True
-		if currentFollowingPageDom.find("li", {"class": "pagination-next"}):
-			pageNo = currentFollowingPageDom.find("li", {"class": "pagination-next"})
-			currentFollowingPageUrl = f"https://www.last.fm/user/{username}/following{pageNo.a['href']}"
-			currentFollowingPageDom = getDom(getResponse(currentFollowingPageUrl))
+		following = following_dom.find_all(attrs={"class": "user-list-name"})
+		for userName in following: # Bir sayfada max 30
+			userName = userName.text.strip()
+			followingDict[userName] = True
+		if following_dom.find("li", {"class": "pagination-next"}):
+			pageNo = following_dom.find("li", {"class": "pagination-next"})
+			currentFollowingPageUrl = f"https://www.last.fm/user/{getUsername(following_dom)}/following{pageNo.a['href']}"
+			following_dom = getDom(getResponse(currentFollowingPageUrl)) # current followers page dom
 		else:
 			return followingDict
 	
-def getUserFollowers(_followersDom):
+def getUserFollowers(followers_dom):
 	if defStatus:
 		print(f'Process: {getUserFollowers.__name__}')
 
 	followersDict = {}
-	currentFollowersPageDom = _followersDom
-	username = getUsername(currentFollowersPageDom)
 	while True:
-		followers = currentFollowersPageDom.find_all(attrs={"class": "user-list-name"})
-		for f in followers: # Bir sayfada max 30
-			f_username = f.text.strip()
-			followersDict[f_username] =  True
-		if currentFollowersPageDom.find("li", {"class": "pagination-next"}):
-			pageNo = currentFollowersPageDom.find("li", {"class": "pagination-next"})
-			currentFollowersPageUrl = f"https://www.last.fm/user/{username}/followers{pageNo.a['href']}"
-			currentFollowersPageDom = getDom(getResponse(currentFollowersPageUrl))
+		followers = followers_dom.find_all(attrs={"class": "user-list-name"})
+		for userName in followers: # Bir sayfada max 30
+			userName = userName.text.strip()
+			followersDict[userName] =  True
+		if followers_dom.find("li", {"class": "pagination-next"}):
+			pageNo = followers_dom.find("li", {"class": "pagination-next"})
+			currentFollowersPageUrl = f"https://www.last.fm/user/{getUsername(followers_dom)}/followers{pageNo.a['href']}"
+			followers_dom = getDom(getResponse(currentFollowersPageUrl)) # current followers page dom
 		else:
 			return followersDict
 
-def getUserGT(_following, _followers):
+def getUserGT(following_box, followers_box):
 	if defStatus:
 		print(f'Process: {getUserGT.__name__}')
 
-	user_gt = {}
-	for user in _following:
-		if user in _followers:
-			user_gt[user] = True
+	userGt = {}
+	for userName in following_box:
+		if userName in followers_box:
+			userGt[userName] = True
 		else:
-			user_gt[user] = False
-	return user_gt
+			userGt[userName] = False
+	return userGt
 
-def getProfileSince(_profileDom):
+def getProfileSince(profile_dom):
 	if defStatus:
 		print(f'Process: {getProfileSince.__name__}')
 
-	profileSince = _profileDom.find("span", {"class":"header-scrobble-since"})
+	profileSince = profile_dom.find("span", {"class":"header-scrobble-since"})
 	return profileSince.text.partition("• scrobbling since")[2].strip() # Sonrasını al
 
-def getLastScrobs(_profileDom, _x):
+def getLastScrobs(profile_dom, get_count):
 	if defStatus:
 		print(f'Process: {getLastScrobs.__name__}')
 
 	lastTracks = {}
-	for x in range(_x): # X kadar al.
+	for x in range(get_count): # X kadar al.
 		try:
-			lastTrackDom = _profileDom.find_all("tr", {"class":"chartlist-row chartlist-row--with-artist chartlist-row--with-buylinks js-focus-controls-container"})[x]
+			lastTrackDom = profile_dom.find_all("tr", {"class":"chartlist-row chartlist-row--with-artist chartlist-row--with-buylinks js-focus-controls-container"})[x]
 			lastTrackSongName = lastTrackDom.find("td", {"class":"chartlist-name"}).text.strip()
 			lastTrackArtist = lastTrackDom.find("td", {"class":"chartlist-artist"}).text.strip()
 			lastTrackDate = lastTrackDom.find("td", {"class":"chartlist-timestamp"}).text.strip()
 			lastTracks[x] = [lastTrackSongName,lastTrackArtist,lastTrackDate]
-		except Exception as e:
+		except:
 			lastTracks =  None
 			break
 	return lastTracks
@@ -365,34 +370,34 @@ def getDictValueCount(dicti, key):
 
 	return sum(key for _ in dicti.values() if _)
 
-def getFollowDict(_following, _followers, _fb):
+def getFollowDict(following_box, followers_box, followback_box):
 	if defStatus:
 		print(f'Process: {getFollowDict.__name__}')
 
 	f = {}
-	for username in _following:
+	for username in following_box:
 		f[username] = {}
 		f[username]['following'] = True
-		if username in _followers:
+		if username in followers_box:
 			f[username]['follower'] = True # 2. true takip ettiği / false etmediği
-			f[username]['user_fb'] = _fb[username]
+			f[username]['user_fb'] = followback_box[username]
 		else:
 			f[username]['follower'] = False
-			f[username]['user_fb'] = _fb[username]
+			f[username]['user_fb'] = followback_box[username]
 		f[username]['link'] = f'https://last.fm/user/{username}'
-	for username in _followers:
+	for username in followers_box:
 		f[username] = {}
 		f[username]['follower'] = True
-		if username in _following:
+		if username in following_box:
 			f[username]['following'] = True # 2. true takip ettiği / false etmediği
-			f[username]['user_fb'] = _fb[username]
+			f[username]['user_fb'] = followback_box[username]
 		else:
 			f[username]['following'] = False
 			f[username]['user_fb'] = False
 		f[username]['link'] = f'https://last.fm/user/{username}'
 	return f
 
-def getTodayListening(_username):
+def getTodayListening(user_name):
 	if defStatus:
 		print(f'Process: {getTodayListening.__name__}')
 
@@ -402,15 +407,15 @@ def getTodayListening(_username):
 	todayTracks = {}
 	todayArtist = []
 	while True:
-		todayListeningUrl = f'https://www.last.fm/user/{_username}/library/artists?from={today}&rangetype=1day&page={pageNo}'
+		todayListeningUrl = f'https://www.last.fm/user/{user_name}/library/artists?from={today}&rangetype=1day&page={pageNo}'
 		todayListeningDom = getDom(getResponse(todayListeningUrl))
 		try:
 			todayListeningDomTracks = todayListeningDom.find_all("tr", "chartlist-row")
 			for i in todayListeningDomTracks:
-				artist_name = i.find("td","chartlist-name").text.strip()
-				artist_count = i.find("span","chartlist-count-bar-value").text.strip()
-				todayArtist.append(artist_name)
-				todayTracks[artist_name] = artist_count[:artist_count.rfind(' ')] # Boşluğun hemen öncesine kadar al. (123 scrobbles)
+				artistName = i.find("td","chartlist-name").text.strip()
+				artistCount = i.find("span","chartlist-count-bar-value").text.strip()
+				todayArtist.append(artistName)
+				todayTracks[artistName] = artistCount[:artistCount.rfind(' ')] # Boşluğun hemen öncesine kadar al. (123 scrobbles)
 		except:
 			pass # Bir hata gerçekleşirse dict boş gönderilir.
 
@@ -419,24 +424,24 @@ def getTodayListening(_username):
 		else:
 			return todayArtist, todayTracks
 
-def getArtistAllTimeCount(_username, _artist, process_loop=None):
+def getArtistAllTimeCount(user_name, artists_box, process_loop=None):
 	if defStatus:
 		print(f'Process: {getArtistAllTimeCount.__name__}')
 
-	if isinstance(_artist, dict): # Sözlük gönderildiyse keys ile işlem yapılır
-		_artist = _artist.keys()
+	if isinstance(artists_box, dict): # Sözlük gönderildiyse keys ile işlem yapılır
+		artists_box = artists_box.keys()
 
 	artistCount = {}
-	for artist_name in _artist:
+	for artistName in artists_box:
 		if process_loop != None:
 			if process_loop != 0:
 				process_loop -= 1
 			else:
 				break
-		artistCountUrl = f'https://www.last.fm/user/{_username}/library/music/{artist_name}?date_preset=ALL'
+		artistCountUrl = f'https://www.last.fm/user/{user_name}/library/music/{artistName}?date_preset=ALL'
 		artistCountDom = getDom(getResponse(artistCountUrl))
 		artist_scrobbles = artistCountDom.find_all("p", {"class":"metadata-display"})[0].text
-		artistCount[artist_name] = artist_scrobbles # library_header_title, metadata_display
+		artistCount[artistName] = artist_scrobbles # library_header_title, metadata_display
 	return artistCount
 
 def getDictKeyNo(key, d): # key, dict
@@ -450,7 +455,7 @@ def getDictKeyNo(key, d): # key, dict
 
 ## -- PRINT DEFS --
 
-def printStatus(upi, _react): # printStatus(userProfileInfos, _react)
+def printStatus(upi, react): # printStatus(userProfileInfos, react)
 	print(f'\n*** {time.strftime("%H:%M:%S")} ***')
 	upi_acot = upi['artist_count_alltime']
 	upi_lts = upi["last_tracks"]
@@ -487,7 +492,7 @@ def printStatus(upi, _react): # printStatus(userProfileInfos, _react)
 	print(f'Artists: {upi_ac} | ', end ="")
 	print(f'Loved Tracks: {upi_lc}'),
 
-	if _react:
+	if react:
 		refresh_time = 5
 		time.sleep(refresh_time) # 5 sec
 		print(f'\nIt will be checked again in {refresh_time} seconds..')
